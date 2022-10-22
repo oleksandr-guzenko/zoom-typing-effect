230fe70b5b766801e723bb04fab650d21cb67e26
dragonwarrior980706 <dragon.warrior980706@gmail.com>
2022-10-23 07:24:40 +0900

    completed

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let i = 0;
 let txt = 'Bring in \'da noise!'; /* The text */
 let speed = 60; /* The speed/duration of the effect in milliseconds */
 let fontSize = 10;
+let scale = 100;
 const el = document.getElementsByClassName("loader")[0];
+const noise = $('body');
 
-$(window).on('load', addNoise);
-// setTimeout(() => {
-//   addNoise();
-// }, 1000);
-// addNoise();
+// $(window).on('load', addNoise);
+setTimeout(() => {
+  addNoise();
+}, 1000);
 function addNoise() {
   $('.loader').text('');
   $('.noise-wrapper').css('opacity',1);
@@ -20,9 +21,13 @@ function typeWriter() {
     if (i < txt.length) {
         el.innerHTML += txt.charAt(i);
         el.style.fontSize = fontSize + 'vw';
+        $(noise).css({backgroundSize: `${scale}% ${scale}%`});
+
         i++;
         speed += 10;
         fontSize += 0.4;
+        scale += 4;
+
         setTimeout(typeWriter, speed);
     }
     }
